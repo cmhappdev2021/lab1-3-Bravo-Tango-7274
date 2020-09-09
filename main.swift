868128793d6071a63ec23209009497bf64ad4efa
BravoTango7274 <aaronritch@att.net>
2020-09-09 18:56:11 +0000

    Finished multipliedAsIntegers

--- main.swift
+++ main.swift
@@ -99,17 +99,22 @@ print(4*9-6/2)
 // In a separate print statement, add in the necessary parentheses so that the subtraction is prioritized over the multiplication and division.
 print(4 * (9 - 6) / 2)
 // If you completed the Fitness Calculations exercise, you calculated an average heart rate to display to the user. However, using proper order of operations you can do this in fewer steps. Create three separate heart rate constants, all of type Double, with values between 60 and 100. Then create a constant equal to the average heart rate. If you use correct order of operations you can do the heart calculation in one line.
-
-
+let heartRate_1 = 74
+let heartRate_2 = 81
+let heartRate_3 = 99
+let averageHeartRate = (heartRate_1+heartRate_2+heartRate_3)/3
+print(averageHeartRate)
 // One feature you might want to give users is to display their current body temperature. Create a constant tempInFahrenheit equal to 98.6.
-
-
+let tempInFahrenheit = 98.6
+print(tempInFahrenheit)
 // You may want to also show the temperature in celsius. You can convert fahrenheit to celsius by taking tempInFahrenheit and subtracting 32, then multiplying the result by (5.0/9.0). Create a constant tempInCelsius that calculates in one line the temperature in celsius.
-
-
+let tempInCelsius = (tempInFahrenheit-32)*(5.0/9.0)
+print(tempInCelsius)
 // Create an integer constant x with a value of 10, and a double constant y with a value of 3.2. Create a constant multipliedAsIntegers equal to x times y. Does this compile? If not, fix it by converting your Double to an Int in the mathematical expression. Print the result.
-
-
+let x: Int = 10
+let y: Double = 3.2
+let multipliedAsIntegers = x*Int(y)
+print(multipliedAsIntegers)
 // Create a constant multipliedAsDoubles equal to x times y, but this time convert the Int to a Double in the expression. Print the result.
 
 
