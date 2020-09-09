b842227d1b135982d9c9b8b0c90e28186c2ac953
BravoTango7274 <aaronritch@att.net>
2020-09-09 18:31:22 +0000

    Finished the averageHRD

--- main.swift
+++ main.swift
@@ -38,8 +38,13 @@ let addedHR = heartRate1 + heartRate2 + heartRate3
 let averageHR = addedHR / 3
 print(averageHR)
 // Now create three more constants, heartRate1D, heartRate2D, and heartRate3D, equal to the same values as heartRate1, heartRate2, and heartRate3. These new constants should be of type Double. Create a constant addedHRD equal to the sum of all three heart rates. Create a constant called averageHRD that equals the addedHRD divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
-
-
+let heartRate1D: Double = 69
+let heartRate2D: Double = 83
+let heartRate3D: Double = 95
+let addedHRD = heartRate1D+heartRate2D+heartRate3D
+let averageHRD = addedHRD/3
+print(averageHRD)
+print("This does differ because the average of the heart rates were not an exact digit so it rounded down, but now that they are doubles, it gave the exact decimal")
 // Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. Create constants steps and goal. Both will need to be of type Double so that you can perform accurate calculations. steps should be assigned the value 3,467, and goal should be assigned 10,000. Create a constant percentOfGoal that equals an expression that evaluates to the percent of the goal that has been achieved so far.
 
 
